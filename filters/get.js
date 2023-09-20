92c0be9edec0a1be4b9f45c406a3a35efe4af88d
Jonny Gerig Meyer <jonny@oddbird.net>
2023-09-20 11:47:48 -0400

    Refactor to avoid mutating pages; remove used filters

--- filters/get.js
+++ filters/get.js
@@ -2,43 +2,15 @@ const getDate = (page) => page.data.changes
   ? page.data.changes.sort((a, b) => b.time - a.time).at(0).time
   : page.data.draft || page.data.created || page.date;
 
-const upDated = (page) => {
-  page.date = getDate(page);
-  return page;
-}
-
 const byDate = (collection) => collection
-  .map((page) => upDated(page))
-  .sort((a, b) => b.date - a.date);
+  .sort((a, b) => getDate(b) - getDate(a));
 
 const latest = (collection) => {
-  return byDate(collection).at(0).date;
+  return getDate(byDate(collection).at(0));
 };
 
 const noSelf = (collection, self) =>
   collection.filter((page) => page.url !== self.url);
-
-const byStatus = (collection) => {
-  const draft = [];
-  const active = [];
-  const archive = [];
-
-  byDate(collection).forEach(page => {
-    if (page.data.archive) {
-      archive.push(page);
-    } else if (page.data.draft) {
-      draft.push(page);
-    } else {
-      active.push(page);
-    }
-  });
-
-  return {
-    draft, active, archive,
-  }
-}
-
-const getStatus = (collection, status) => byStatus(collection)[status];
 
 const getIndex = (collection, tag) =>
   collection.find((page) => page.data.index === tag);
@@ -48,12 +20,9 @@ const getPage = (collection, page) =>
 
 module.exports = function (eleventyConfig) {
   eleventyConfig.addFilter('getDate', getDate);
-  eleventyConfig.addFilter('upDated', upDated);
   eleventyConfig.addFilter('byDate', byDate);
   eleventyConfig.addFilter('latest', latest);
   eleventyConfig.addFilter('noSelf', noSelf);
-  eleventyConfig.addFilter('byStatus', byStatus);
-  eleventyConfig.addFilter('getStatus', getStatus);
   eleventyConfig.addFilter('getIndex', getIndex);
   eleventyConfig.addFilter('getPage', getPage);
 
